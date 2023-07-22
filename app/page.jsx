38772b633155c350f6cdfb895fe2392f0a92cdfd
midCenturyDude7 <mgriffes@gmail.com>
2023-07-22 16:48:53 -0700

    Added sign-in, sign-up and sign-out functionality

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <h1 className="text-2xl font-bold mb-5">Welcome</h1>
+      <p className="mb-5">
+        This is a demo site for a simple Next.js and React application using
+        authentication from Clerk
+      </p>
     </>
   );
 }
