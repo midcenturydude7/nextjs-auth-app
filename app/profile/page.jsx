d4ac6a5b7d72c0c45ef4ac993fe8de3ecfd24d31
midCenturyDude7 <mgriffes@gmail.com>
2023-07-22 16:56:53 -0700

    Added sign-in, sign-up and sign-out functionality

---
+++ app/profile/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { UserProfile } from "@clerk/nextjs";
+
+const ProfilePage = () => {
+  return (
+    <>
+      <UserProfile />
+    </>
+  );
+};
+
+export default ProfilePage;
